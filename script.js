13228ae72776c1ac18fcc4fec577189756553ec4
Austin-Senna <austinsenna@gmail.com>
2025-06-03 12:12:56 +0700

    added read button

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const myLibrary = [];
 let container = document.querySelector(".container");
 let addButton = document.querySelector("#add-button")
 
-function Book(title, author, pages, id) {
+function Book(title, author, pages, isRead, id) {
     if (!new.target) {
         throw Error("Make it a new target.")
     }
@@ -11,11 +11,12 @@ function Book(title, author, pages, id) {
     this.title = title;
     this.author = author;
     this.pages = pages;
+    this.isRead = isRead;
     this.id = id;
 }
 
-function addBookToLibrary(title, author, pages) {
-    let newBook = new Book(title, author, pages, crypto.randomUUID());
+function addBookToLibrary(title, author, pages, isRead) {
+    let newBook = new Book(title, author, pages, isRead, crypto.randomUUID());
     myLibrary.push(newBook);
 }
 
@@ -28,27 +29,64 @@ function displayBook(book) {
     author.innerText = book.author;
     let pages = document.createElement("p");
     pages.innerText = `Pages: ${book.pages}`;
+    let read = document.createElement("p");
+    read.innerText = generateReadText(book.isRead);
+
     newBook.appendChild(title);
     newBook.appendChild(author);
     newBook.appendChild(pages);
+    newBook.appendChild(read);
+
+    let buttonContainer = document.createElement("div");
+    buttonContainer.classList.add("buttonContainer")
+
+    let id = book.id
+    let readButton = document.createElement("button");
+    readButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>book-open</title><path d="M13,12H20V13.5H13M13,9.5H20V11H13M13,14.5H20V16H13M21,4H3A2,2 0 0,0 1,6V19A2,2 0 0,0 3,21H21A2,2 0 0,0 23,19V6A2,2 0 0,0 21,4M21,19H12V6H21" /></svg>'
+    readButton.setAttribute("type", "button");
+    readButton.readTextElement = read;
+    readButton.addEventListener("click", (e) => toggleRead(e, id));
+    buttonContainer.appendChild(readButton);
 
     let deleteButton = document.createElement("button");
     deleteButton.innerText = "X";
     deleteButton.setAttribute('type', 'button');
-    deleteButton.addEventListener("click", removeParent)
-    newBook.appendChild(deleteButton);
+    deleteButton.classList.add("delete");
+    deleteButton.addEventListener("click", (e) => removeParent(e, id))
+    buttonContainer.appendChild(deleteButton);
+
+    newBook.appendChild(buttonContainer);
 
     container.appendChild(newBook);
 }
 
-function removeParent(e){
-    let parent = e.target.parentElement;
-    parent.remove();
+function generateReadText(boolRead) {
+    if (boolRead) {
+        return "Read."
+    }
+    return "Not read."
 }
 
+function toggleRead(e, id) {
+    var toggleIndex = myLibrary.findIndex(book => book.id == id);
+    myLibrary[toggleIndex].isRead = !myLibrary[toggleIndex].isRead;
+
+    var  newRead = myLibrary[toggleIndex].isRead;
+    e.currentTarget.readTextElement.innerText = generateReadText(newRead);
+}
+
+function removeParent(e, id){
+    let parent = e.target.parentElement.parentElement;
+    parent.remove();
+
+    var removeIndex = myLibrary.findIndex(book => book.id == id);
+    myLibrary.splice(removeIndex, 1);
+}
+
+
 function main() {
-    addBookToLibrary("Austin's Love Story", "Austin Senna", 123);
-    addBookToLibrary("Javier's Coding Journey", "Javier Enrique Wong", 301);
+    addBookToLibrary("Austin's Love Story", "Austin Senna", 123, false);
+    addBookToLibrary("Javier's Coding Journey", "Javier Enrique Wong", 301, true);
     
     for (const book of myLibrary) {
         displayBook(book);
@@ -56,4 +94,10 @@ function main() {
 }
 
 main();
+
 addButton.addEventListener("click", displayForm);
+
+function displayForm(){
+    let form = document.createElement("form");
+    
+}
